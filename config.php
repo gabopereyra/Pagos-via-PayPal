b27227dfe04fdb745524346802c83ec6da5df106
Gabriel Pereyra <pereyra.g@outlook.com>
2020-10-02 20:27:13 -0300

    Se genera configuracion de API

--- config.php
+++ config.php
@@ -1,3 +1,12 @@
-<?php 
+<?php
 
-include 'SDK/autoload.php'
+include 'SDK/autoload.php';
+
+$apiContext = new \PayPal\Rest\ApiContext(
+    new \PayPal\Auth\OAuthTokenCredential(
+        'AT_7_mC5xABFviXKmNHUTqdTf92k-hKOQBCxwlort4sX1zNu1wf_B2TFCLdkZMiLLww8iP6fegx6nzIQ',
+        //CLiente ID
+        'EHAOZWsaq-Fs1PnwNGla5kIc4Cu4xQRulvQxDdsHMrv1bYDFxkCH2aBrzHtIs8oIlqpGjnDDif0W5SUG'
+        //Secret
+    )
+);
