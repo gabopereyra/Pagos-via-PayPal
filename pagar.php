d2f34123d03fc0f31b434b4b194187ab176143ce
Gabriel Pereyra <pereyra.g@outlook.com>
2020-10-02 21:15:13 -0300

    Agregando Detalles y Cantidad a nuestro Pago

--- pagar.php
+++ pagar.php
@@ -1,7 +1,10 @@
 <?php
 
+use PayPal\Api\Amount;
+use PayPal\Api\Details;
 use PayPal\Api\Payer;
 use PayPal\Api\Item;
+use PayPal\Api\ItemList;
 
 if (!isset($_POST['producto'], $_POST['precio'])) {
     exit("Hubo un error");
@@ -24,3 +27,12 @@ $articulo->setName($producto)
     ->setCurrency('USD')
     ->setQuantity(1)
     ->setPrice($total);
+
+$listaArticulos = new ItemList();
+$listaArticulos->setItems(array($articulo));
+
+$detalles = new Details();
+$detalles->setShipping($envio)->setSubtotal($precio);
+
+$cantidad = new Amount();
+$cantidad->setCurrency('USD')->setTotal($total)->setDetails($detalles);
